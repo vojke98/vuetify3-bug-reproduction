a691b65a20fc98c315385babb01a5b6990ff039b
vojke98 <dejanvojinovic@yahoo.com>
2023-09-25 10:06:58 +0200

    :tada: Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+        <v-app-bar>
+            <div class="d-flex ease-width" :style="{ width: `${width}px` }">
+                <v-autocomplete :menu-icon="isFocused ? 'mdi-menu-down' : null" @mouseenter="width = 300" @mouseover="width = 300" @mouseleave="width = isFocused ? 300 : 45" @focus="focus()" @blur="blur()" placeholder="Search views" variant="solo-filled" density="compact" prepend-inner-icon="mdi-magnify" rounded hide-details></v-autocomplete>
+            </div>
+        </v-app-bar>
+    </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const width = ref(45);
+const isFocused = ref(false);
+
+function focus() {
+    isFocused.value = true;
+    width.value = 300;
+}
+
+function blur() {
+    isFocused.value = false;
+    width.value = 45;
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.ease-width {
+    transition: width 0.5s ease-in-out;
 }
 </style>
