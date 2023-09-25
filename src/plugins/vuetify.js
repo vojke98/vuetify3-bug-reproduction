d69a799280db39ab34907dbe68bdef59b05a9a0c
vojke98 <dejanvojinovic@yahoo.com>
2023-09-25 10:11:05 +0200

    :fire: Removed unnecessary code

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,45 +6,7 @@ import * as directives from 'vuetify/directives';
 import "@mdi/font/css/materialdesignicons.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-const customLight = {
-    dark: false,
-    colors: {
-        primary: '#2196F3',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107',
-    },
-}
-
-const customDark = {
-    dark: true,
-    colors: {
-        primary: '#2196F3',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107',
-    },
-}
-
-/* const settings = {
-    theme: {
-        defaultTheme: 'dark'
-    }
-} */
-
 export default createVuetify({
     components,
-    directives,
-    theme: {
-        themes: {
-            customLight,
-            customDark
-        }
-    }
+    directives
 });
